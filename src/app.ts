31c369c90399421d080ebc4c9909b1c2d6ed24ca
Xnuk Shuman <admin@xnu.kr>
2020-04-16 16:40:02 +0900

    prettier-eslint

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,12 @@ import { qrcode } from './qrcode'
 const img = element('img')
 const input = element('input')
 
-const update = (): unknown => requestAnimationFrame(() => {
-	img.src = qrcode(input.value || '')
-})
+const update = (): unknown =>
+	requestAnimationFrame(() => {
+		img.src = qrcode(input.value || '')
+	})
 
-;[
+const inputEvents = [
 	'keydown',
 	'keyup',
 	'change',
@@ -16,7 +17,9 @@ const update = (): unknown => requestAnimationFrame(() => {
 	'paste',
 	'compositionupdate',
 	'compositionend',
-].map(key => input.addEventListener(key, update, false))
+]
+
+inputEvents.map(key => input.addEventListener(key, update, false))
 
 const hashchange = (): void => {
 	input.value = WINDOW.location.hash.substring(1)
